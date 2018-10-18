dca964111adc34d9afdcb0d658f6aaab0f84d360
PavitKaur05 <pkaur.2364@yahoo.com>
2018-10-18 19:40:53 +0530

    Solve Issue#37

---
+++ introduction/Pavit/Issue#37/main.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+ int num,fact,i;
+ fact=1;
+ cout<<"Program to calculate factorial of a number"<<endl;
+ cout<<"Enter the no"<<endl;
+ cin>>num;//statement to get value from user
+ for(i=1;i<=num;i++)
+  fact=fact*i;
+ cout<<"The factorial of number = "<<fact<<endl;
+ return 0;
+}

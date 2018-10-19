c65be498158c16179a72fc15b0f0e5e9f8793c1c
PavitKaur05 <pkaur.2364@yahoo.com>
2018-10-19 17:33:19 +0530

    Solve issue#42

---
+++ introduction/Pavit/issue#42.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include<string.h>
+#include<stdio.h>
+using namespace std;
+
+int main()
+{
+  char st[50];
+  int l,i,k=0,f=1;
+  cout<<"Program to check for palindrome for an entered string in C++ "<<endl;
+  cout<<"Ënter the string"<<endl;
+  gets(st);
+  l=strlen(st);
+  for(i=l-1;i>=0;i--)
+{
+    if(st[i]!=st[k])
+    {
+        f=0;
+        break;
+    }
+    k++;
+}
+if(f==1)
+    cout<<"It is a palindrome string"<<endl;
+else
+    cout<<"It is not a palindrome string"<<endl;
+return 0;
+}
